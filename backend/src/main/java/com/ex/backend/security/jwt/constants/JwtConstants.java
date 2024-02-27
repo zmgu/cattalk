5c3870d9e6802dea3603dbd96d933a4f7983c28c
zmgu <manggu94@gmail.com>
2024-02-27 18:23:00 +0900

    JwtConstants 생성

---
+++ backend/src/main/java/com/ex/backend/security/jwt/constants/JwtConstants.java
@@ -0,0 +1,10 @@
+package com.ex.backend.security.jwt.constants;
+
+public class JwtConstants {
+
+    public static final String AUTH_LOGIN_URL = "/login";
+    public static final String ACCESS_TOKEN_HEADER = "AccessToken";
+    public static final String REFRESH_TOKEN_HEADER = "RefreshToken";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String TOKEN_TYPE = "JWT";
+}
