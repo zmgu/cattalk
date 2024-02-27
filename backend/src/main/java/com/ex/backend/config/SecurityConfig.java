f5333d79293aaa32c0957f189188b3e4c213a929
zmgu <manggu94@gmail.com>
2024-02-27 18:22:15 +0900

    SecurityConfig 생성

---
+++ backend/src/main/java/com/ex/backend/config/SecurityConfig.java
@@ -0,0 +1,111 @@
+package com.ex.backend.config;
+
+import com.ex.backend.security.jwt.filter.JwtRequestFilter;
+import com.ex.backend.security.jwt.provider.JwtProvider;
+import com.ex.backend.security.oauth2.handler.OAuth2SuccessHandler;
+import com.ex.backend.user.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true, securedEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2SuccessHandler oAuth2SuccessHandler;
+    private final JwtProvider jwtProvider;
+
+    // 시큐리티 설정
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        log.info("시큐리티 설정...");
+
+        //csrf, From 로그인 방식, HTTP Basic 인증 방식 disable
+        http
+                .csrf((csrf) -> csrf.disable())
+                .formLogin((login) -> login.disable())
+                .httpBasic((basic) -> basic.disable());
+
+//        http
+//                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+//
+//                    @Override
+//                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+//
+//                        CorsConfiguration configuration = new CorsConfiguration();
+//
+//                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+//                        configuration.setAllowedMethods(Collections.singletonList("*"));
+//                        configuration.setAllowCredentials(true);
+//                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+//                        configuration.setMaxAge(3600L);
+//
+//                        configuration.setExposedHeaders(Collections.singletonList("Set-Cookie"));
+//                        configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+//
+//                        return configuration;
+//                    }
+//                }));
+
+        // 필터 설정 ✅
+        http
+//                .addFilterAt(new JwtAuthenticationFilter(authenticationManager, jwtProvider)
+//                        , UsernamePasswordAuthenticationFilter.class)
+
+                .addFilterBefore(new JwtRequestFilter(jwtProvider)
+                        , UsernamePasswordAuthenticationFilter.class)
+        ;
+        //oauth2
+        http
+                .oauth2Login((oauth2) -> oauth2
+                        .userInfoEndpoint((userInfoEndpointConfig) -> userInfoEndpointConfig
+                                .userService(customOAuth2UserService))
+                        .successHandler(oAuth2SuccessHandler)
+                );
+        // 인가 설정 ✅
+        http.authorizeHttpRequests( authorizeRequests ->
+                authorizeRequests
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                        .requestMatchers("/").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/users/**").permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+        );
+
+        // 인증 방식 설정 ✅
+//        http.userDetailsService(customUserDetailService);
+
+        return http.build();
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    private AuthenticationManager authenticationManager;
+
+    @Bean
+    public AuthenticationManager authenticationManager
+            (AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        this.authenticationManager = authenticationConfiguration.getAuthenticationManager();
+        return authenticationManager;
+    }
+}
