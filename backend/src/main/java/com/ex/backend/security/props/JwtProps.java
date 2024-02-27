bbac37628a83739cbbd90a0741da996ad8ca341d
zmgu <manggu94@gmail.com>
2024-02-27 18:23:11 +0900

    JwtProps 생성

---
+++ backend/src/main/java/com/ex/backend/security/props/JwtProps.java
@@ -0,0 +1,15 @@
+package com.ex.backend.security.props;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+@ConfigurationProperties("com.ex.backend")       // application.properties 의 하위 속성 경로 지정
+public class JwtProps {
+
+    private String secretKey;
+    private int accessTokenExpiredTime;
+    private int refreshTokenExpiredTime;
+}
