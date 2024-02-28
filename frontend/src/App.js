705bf91a0b98aff82c57a1219e047d8bb81208fc
zmgu <manggu94@gmail.com>
2024-02-28 20:18:29 +0900

    라우터 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'; // BrowserRouter를 사용합니다.
+import Login from './pages/login/Login';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router> {/* Router를 추가합니다. */}
+      <Login />
+    </Router>
   );
 }
 
