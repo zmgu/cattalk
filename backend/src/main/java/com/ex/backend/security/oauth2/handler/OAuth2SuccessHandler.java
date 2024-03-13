653850827055a9b6d6541ffabecb670e1a2021ca
zmgu <manggu94@gmail.com>
2024-03-13 17:58:15 +0900

    OAuth2SuccessHandler 수정중

--- backend/src/main/java/com/ex/backend/security/oauth2/handler/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/ex/backend/security/oauth2/handler/OAuth2SuccessHandler.java
@@ -11,7 +11,7 @@ import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
@@ -20,18 +20,18 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
+import java.util.logging.Logger;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
     private final JwtProvider jwtProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final Logger logger = Logger.getLogger(OAuth2SuccessHandler.class.getName());
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-
 
         PrincipalDetails user = (PrincipalDetails) authentication.getPrincipal();
         String username = user.getUsername();
@@ -41,32 +41,30 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
 
-        // 💍 JWT 토큰 생성 요청
-        String accessToken = jwtProvider.createAccessToken(username, role);
-        String refreshToken = jwtProvider.createRefreshToken();
+        String accessToken = jwtProvider.createToken("access" ,username, role, 600000l);
+        String refreshToken = jwtProvider.createToken("refresh" ,username, role, 86400000L);
 
-        log.info("accessToken" + accessToken);
-        log.info("refreshToken" + refreshToken);
+        logger.info("accessToken" + accessToken);
+        logger.info("refreshToken" + refreshToken);
 
+
+        // Redis에 refreshToken 저장
         refreshTokenRepository.save(new RefreshToken(refreshToken, username));
 
-        // 💍 { Authorization : Bearer + {jwt} }
-        response.addCookie(createCookie(JwtConstants.ACCESS_TOKEN_HEADER, accessToken, false));
-        response.addCookie(createCookie(JwtConstants.REFRESH_TOKEN_HEADER, refreshToken, true));
 
-        response.addHeader(JwtConstants.ACCESS_TOKEN_HEADER, JwtConstants.TOKEN_PREFIX + accessToken);
-        response.setStatus(200);
+        response.addCookie(createCookie(JwtConstants.REFRESH_TOKEN_HEADER, refreshToken));  // 쿠키에 refreshToken 저장
+        response.addHeader(JwtConstants.ACCESS_TOKEN_HEADER, JwtConstants.TOKEN_PREFIX + accessToken);  // Header에 accessToken 저장
+        response.setStatus(HttpStatus.OK.value());
         response.sendRedirect("http://localhost:3000/");
     }
 
-    private Cookie createCookie(String tokenHeader, String token, Boolean httpOnly) {
+    private Cookie createCookie(String tokenHeader, String token) {
 
         Cookie cookie = new Cookie(tokenHeader, token);
         cookie.setMaxAge(600*60*60);
-
         //cookie.setSecure(true);
-        cookie.setPath("/");
-        cookie.setHttpOnly(httpOnly);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
 
         return cookie;
     }
