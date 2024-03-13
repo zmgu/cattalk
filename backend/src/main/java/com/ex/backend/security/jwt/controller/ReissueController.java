158518d5e88bc7bd41826e41521155eed71caa17
zmgu <manggu94@gmail.com>
2024-03-13 17:59:15 +0900

    ReissueController 생성

---
+++ backend/src/main/java/com/ex/backend/security/jwt/controller/ReissueController.java
@@ -0,0 +1,22 @@
+package com.ex.backend.security.jwt.controller;
+
+import com.ex.backend.security.jwt.service.ReissueService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReissueController {
+
+    private final ReissueService reissueService;
+
+    @PostMapping("/reissue")
+    public ResponseEntity<?> reissue(HttpServletRequest request, HttpServletResponse response) {
+
+        return reissueService.reissue(request, response);
+    }
+}
