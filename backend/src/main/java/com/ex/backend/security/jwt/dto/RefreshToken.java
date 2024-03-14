0a225dff9600d0772e60ddb11b98963437bbedcb
zmgu <manggu94@gmail.com>
2024-03-14 17:38:31 +0900

    필드 수정

--- backend/src/main/java/com/ex/backend/security/jwt/dto/RefreshToken.java
+++ backend/src/main/java/com/ex/backend/security/jwt/dto/RefreshToken.java
@@ -1,5 +1,6 @@
 package com.ex.backend.security.jwt.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.redis.core.RedisHash;
@@ -8,15 +9,10 @@ import java.util.UUID;
 
 @Getter @Setter
 @RedisHash(value = "refreshToken", timeToLive = 60*60)
+@AllArgsConstructor
 public class RefreshToken {
 
-    private UUID id;
     private String refreshToken;
     private String username;
 
-    public RefreshToken(String refreshToken, String username) {
-        this.id = UUID.randomUUID();
-        this.refreshToken = refreshToken;
-        this.username = username;
-    }
 }
