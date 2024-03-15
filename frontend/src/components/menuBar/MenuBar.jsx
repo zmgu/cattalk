ebaa0b0d35663c2ddb7529d899796693dbd18d19
zmgu <manggu94@gmail.com>
2024-03-15 17:38:22 +0900

    메인 메뉴바 생성

---
+++ frontend/src/components/menuBar/MenuBar.jsx
@@ -0,0 +1,19 @@
+import './MenuBar.css'
+import { Link } from 'react-router-dom'
+
+const MenuBar = () => {
+    return (
+        <div className='menubar-container'>
+            <div className=''>
+                <Link to="" className=''></Link>
+                <Link to="" className=''></Link>
+                <Link to="" className=''></Link>
+                <Link to="" className=''></Link>
+                <Link to="" className=''></Link>
+            </div>
+        </div>
+    );
+
+}
+
+export default MenuBar;
