cae467fc3976d85321f19c8615aace7094d5085d
zmgu <manggu94@gmail.com>
2024-03-15 17:34:49 +0900

    라우터 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Login from './pages/login/Login';
+import Main from './pages/main/Main';
+import LoginContextProvider from './contexts/LoginContextProvider';
 
 function App() {
   return (
-    <Router>
-      <Login />
-    </Router>
+    <BrowserRouter>
+      <LoginContextProvider>
+        <Routes>
+            <Route path='/' element={ <Main /> } />
+            <Route path="/login" element={ <Login /> }/>    
+        </Routes>
+      </LoginContextProvider>
+    </BrowserRouter>
   );
 }
 
