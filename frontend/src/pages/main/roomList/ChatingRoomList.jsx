684006118f8d02b1ba75931996cc8d54f86b4133
zmgu <manggu94@gmail.com>
2024-03-15 17:40:21 +0900

    채팅방 리스트 페이지 생성

---
+++ frontend/src/pages/main/roomList/ChatingRoomList.jsx
@@ -0,0 +1,14 @@
+import RoomListHeader from "../../../components/roomList/roomListHeader/RoomListHeader";
+import RoomList from "../../../components/roomList/RoomList";
+
+const ChatingRoomList = () => {
+    return (
+        <div className=''>
+            <RoomListHeader />
+            <RoomList />
+        </div>
+
+    );
+}
+
+export default ChatingRoomList;
