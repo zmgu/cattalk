01f43afa4cfc7b9d1648589c4e6e4fcfaef7f42f
zmgu <manggu94@gmail.com>
2024-03-16 23:40:32 +0900

    메뉴바 아이콘 생성

--- frontend/src/components/menuBar/MenuBar.jsx
+++ frontend/src/components/menuBar/MenuBar.jsx
@@ -1,15 +1,15 @@
 import './MenuBar.css'
 import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faComment, faEllipsis } from '@fortawesome/free-solid-svg-icons';
 
 const MenuBar = () => {
     return (
         <div className='menubar-container'>
-            <div className=''>
-                <Link to="" className=''></Link>
-                <Link to="" className=''></Link>
-                <Link to="" className=''></Link>
-                <Link to="" className=''></Link>
-                <Link to="" className=''></Link>
+            <div className='menu-link-box'>
+                <Link to="" className='menu-link'><FontAwesomeIcon icon={faUser} className='menubar-icon'/></Link>
+                <Link to="" className='menu-link'><FontAwesomeIcon icon={faComment} className='menubar-icon'/></Link>
+                <Link to="" className='menu-link'><FontAwesomeIcon icon={faEllipsis} className='menubar-icon'/></Link>
             </div>
         </div>
     );
