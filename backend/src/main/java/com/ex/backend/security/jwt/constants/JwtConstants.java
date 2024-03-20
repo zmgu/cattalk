5bd44b722b6d7ad64442012a3e96a4a8e17d4a8d
zmgu <manggu94@gmail.com>
2024-03-20 18:01:55 +0900

    상수명 변경

--- backend/src/main/java/com/ex/backend/security/jwt/constants/JwtConstants.java
+++ backend/src/main/java/com/ex/backend/security/jwt/constants/JwtConstants.java
@@ -2,9 +2,8 @@ package com.ex.backend.security.jwt.constants;
 
 public class JwtConstants {
 
-    public static final String AUTH_LOGIN_URL = "/login";
-    public static final String ACCESS_TOKEN_HEADER = "AccessToken";
-    public static final String REFRESH_TOKEN_HEADER = "RefreshToken";
+    public static final String ACCESS_TOKEN = "AccessToken";
+    public static final String REFRESH_TOKEN = "RefreshToken";
     public static final String TOKEN_PREFIX = "Bearer ";
-    public static final String TOKEN_TYPE = "JWT";
+    public static final String AUTHORIZATION = "Authorization";
 }
