ec3dead27fba9b479aa0b905858e7c3191d9f283
zmgu <manggu94@gmail.com>
2024-03-20 23:17:58 +0900

    임시 로그아웃 버튼 추가

--- frontend/src/components/roomList/roomListHeader/RoomListHeader.jsx
+++ frontend/src/components/roomList/roomListHeader/RoomListHeader.jsx
@@ -1,9 +1,15 @@
 import './RoomListHeader.css'
+import { LoginContext } from '../../../contexts/LoginContextProvider';
+import { useContext } from 'react';
 
 const RoomListHeader = () => {
+
+    const { logout } = useContext(LoginContext);
+
     return (
         <div className='header-container'>
             <div><h3>채팅</h3></div>
+            <button className='' onClick={ () => logout() }>로그아웃</button>
         </div>
     );
 }
