b1573b6d7f88523eece4cafb674b51b4a3541aca
zmgu <manggu94@gmail.com>
2024-03-20 18:06:25 +0900

    로그 제거

--- backend/src/main/java/com/ex/backend/user/controller/UserController.java
+++ backend/src/main/java/com/ex/backend/user/controller/UserController.java
@@ -3,16 +3,13 @@ package com.ex.backend.user.controller;
 import com.ex.backend.user.dto.PrincipalDetails;
 import com.ex.backend.user.dto.User;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Slf4j
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
@@ -23,15 +20,10 @@ public class UserController {
      * @param principalDetails
      * @return
      */
-    @Secured({"ROLE_USER", "ROLE_ADMIN"})           // USER 권한 설정
     @GetMapping("/info")
     public ResponseEntity<?> userInfo(@AuthenticationPrincipal PrincipalDetails principalDetails) {
 
-        log.info("::::: PrincipalDetails :::::");
-        log.info("PrincipalDetails : "+ principalDetails);
-
         User user = principalDetails.getUser();
-        log.info("user : " + user);
 
         // 인증된 사용자 정보
         if( user != null )
