c098dc5ea2d00ddfe9604024c4ea2c4864cda6a7
zmgu <manggu94@gmail.com>
2024-03-20 18:05:44 +0900

    코드 정리

--- backend/src/main/java/com/ex/backend/security/oauth2/handler/OAuth2SuccessHandler.java
+++ backend/src/main/java/com/ex/backend/security/oauth2/handler/OAuth2SuccessHandler.java
@@ -3,11 +3,12 @@ package com.ex.backend.security.oauth2.handler;
 
 import com.ex.backend.redis.RefreshTokenService;
 import com.ex.backend.security.jwt.constants.JwtConstants;
+import com.ex.backend.security.jwt.cookie.CookieUtil;
 import com.ex.backend.security.jwt.dto.RefreshToken;
+import com.ex.backend.security.jwt.props.JwtProps;
 import com.ex.backend.security.jwt.provider.JwtProvider;
 import com.ex.backend.user.dto.PrincipalDetails;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
@@ -28,6 +29,7 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     private final JwtProvider jwtProvider;
     private final RefreshTokenService refreshTokenService;
+    private final CookieUtil cookieUtil;
     private final Logger logger = Logger.getLogger(OAuth2SuccessHandler.class.getName());
 
     @Override
@@ -35,32 +37,23 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
         PrincipalDetails user = (PrincipalDetails) authentication.getPrincipal();
         String username = user.getUsername();
+        String name = user.getName();
 
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
         GrantedAuthority auth = iterator.next();
         String role = auth.getAuthority();
 
-        String refreshToken = jwtProvider.createToken("RefreshToken" ,username, role, 86400000L);
+        String refreshToken = jwtProvider.createToken(JwtConstants.REFRESH_TOKEN ,username, name, role);
 
         logger.info("refreshToken: " + refreshToken);
 
         // Redis에 refreshToken 저장
         refreshTokenService.createRefreshToken(new RefreshToken(refreshToken, username));
 
-
-        response.addCookie(createCookie(JwtConstants.REFRESH_TOKEN_HEADER, refreshToken));
+        response.addCookie(cookieUtil.createCookie(JwtConstants.REFRESH_TOKEN, refreshToken));
         response.setStatus(HttpStatus.OK.value());
         response.sendRedirect("http://localhost:3000/oauth2");
     }
 
-    private Cookie createCookie(String tokenHeader, String token) {
-
-        Cookie cookie = new Cookie(tokenHeader, token);
-        cookie.setMaxAge(600*60*60);
-        cookie.setPath("/");
-        cookie.setHttpOnly(true);
-
-        return cookie;
-    }
 }
