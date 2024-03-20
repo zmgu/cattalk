44408698b2913b8a20523068cedec13070a64d99
zmgu <manggu94@gmail.com>
2024-03-20 18:04:27 +0900

    CookieUtil 생성

---
+++ backend/src/main/java/com/ex/backend/security/jwt/cookie/CookieUtil.java
@@ -0,0 +1,40 @@
+package com.ex.backend.security.jwt.cookie;
+
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CookieUtil {
+
+    public String getCookieValue(HttpServletRequest request, String cookieName) {
+        if (request.getCookies() == null) {
+            return null;
+        }
+        for (Cookie cookie : request.getCookies()) {
+            if (cookieName.equals(cookie.getName())) {
+                return cookie.getValue();
+            }
+        }
+        return null;
+    }
+
+    public Cookie createCookie(String tokenHeader, String token) {
+
+        Cookie cookie = new Cookie(tokenHeader, token);
+        cookie.setMaxAge(60*60);
+        cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
+    }
+
+    public Cookie deleteCookie(String tokenHeader) {
+
+        Cookie cookie = new Cookie(tokenHeader, null);
+        cookie.setMaxAge(0);
+        cookie.setPath("/");
+
+        return cookie;
+    }
+}
