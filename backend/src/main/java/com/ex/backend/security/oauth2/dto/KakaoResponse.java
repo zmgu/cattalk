087ef316d0971837edf02d05827ad1a2f2727e38
zmgu <manggu94@gmail.com>
2024-03-21 15:05:34 +0900

    KakaoResponse 생성

---
+++ backend/src/main/java/com/ex/backend/security/oauth2/dto/KakaoResponse.java
@@ -0,0 +1,39 @@
+package com.ex.backend.security.oauth2.dto;
+
+import java.util.Map;
+
+public class KakaoResponse implements OAuth2Response {
+
+    private final Map<String, Object> attributes;
+
+    public KakaoResponse(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    @Override
+    public String getProvider() {
+        return "kakao";
+    }
+
+    @Override
+    public String getProviderId() {
+        return attributes.get("id").toString();
+    }
+
+    @Override
+    public String getEmail() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        return kakaoAccount != null ? (String) kakaoAccount.get("email") : null;
+    }
+
+    @Override
+    public String getName() {
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        if (kakaoAccount != null) {
+            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+            return profile != null ? (String) profile.get("nickname") : null;
+        }
+        return null;
+    }
+}
+
