c6d864614b9a3283fab0e6965060cef530c91b8d
zmgu <manggu94@gmail.com>
2024-03-21 17:57:13 +0900

    로그아웃, 채팅방 설정 버튼 추가

--- frontend/src/components/roomList/roomListHeader/RoomListHeader.jsx
+++ frontend/src/components/roomList/roomListHeader/RoomListHeader.jsx
@@ -1,16 +1,40 @@
 import './RoomListHeader.css'
 import { LoginContext } from '../../../contexts/LoginContextProvider';
-import { useContext } from 'react';
+import { useContext, useState } from 'react'; // useState를 추가합니다.
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 
 const RoomListHeader = () => {
-
     const { logout } = useContext(LoginContext);
+    const [showList, setShowList] = useState(false); // 리스트 표시 상태를 관리합니다.
+
+    const toggleList = () => {
+        setShowList(!showList); // 현재 상태의 반대값으로 설정합니다.
+    }
 
     return (
         <div className='header-container'>
-            <div><h3>채팅</h3></div>
-            <button className='' onClick={ () => logout() }>로그아웃</button>
+            <div className='header-title'>
+                <h3>채팅</h3>
+            </div>
+            <div className='header-btn-box'>
+                <button className='' onClick={ () => logout() }>로그아웃</button>
+                <button className='roomlist-header-btn' onClick={toggleList}>
+                    <FontAwesomeIcon icon={faGear} className='roomlist-header-icon' />
+                </button>
+                {/* showList 상태에 따라 리스트를 표시하거나 숨깁니다. */}
+                {showList && (
+                    <div className='roomlist-options'>
+                        {/* 여기에 리스트 아이템을 추가합니다. 예시로 몇 가지를 넣어두었습니다. */}
+                        <button>옵션 1</button>
+                        <button>옵션 2</button>
+                        <button>옵션 3</button>
+                    </div>
+                )}
+            </div>
+
         </div>
+
     );
 }
 
