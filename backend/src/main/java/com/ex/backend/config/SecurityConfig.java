658a3be65912ca39177bfe99a0d9cd4ed7de9555
zmgu <manggu94@gmail.com>
2024-03-22 17:58:16 +0900

    채팅방 페이지 권한 설정 추가

--- backend/src/main/java/com/ex/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/ex/backend/config/SecurityConfig.java
@@ -20,7 +20,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutFilter;
 import org.springframework.web.cors.CorsConfiguration;
@@ -92,6 +91,7 @@ public class SecurityConfig {
                         .requestMatchers("/login").permitAll()
                         .requestMatchers("/auth/reissue").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/oauth2").permitAll()
+                        .requestMatchers("/chat").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/users/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .anyRequest().authenticated()
