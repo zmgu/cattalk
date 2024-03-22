55f598be4d0cfe36de34ad9e9435b3ea6611fc11
zmgu <manggu94@gmail.com>
2024-03-22 18:02:54 +0900

    채팅 화면 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Login from './pages/login/Login';
 import Main from './pages/main/Main';
 import LoginContextProvider from './contexts/LoginContextProvider';
 import OAuth2RedirectHandler from './contexts/OAuth2RedirectHandler.jsx';
+import Chat from './pages/chating/Chat.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
             <Route path='*' element={ <Main /> } />
             <Route path="/login" element={ <Login /> }/>
+            <Route path='/chat' element={ <Chat />}/>
             <Route path='/oauth2' element={<OAuth2RedirectHandler />} />  
         </Routes>
       </LoginContextProvider>
