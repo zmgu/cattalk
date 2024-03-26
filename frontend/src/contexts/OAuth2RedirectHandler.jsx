1fd7f64872bb13a24e63b781f36ba009cd7873c7
zmgu <manggu94@gmail.com>
2024-03-26 23:51:43 +0900

    소셜로그인 로그인 중 화면 생성

--- frontend/src/contexts/OAuth2RedirectHandler.jsx
+++ frontend/src/contexts/OAuth2RedirectHandler.jsx
@@ -1,4 +1,5 @@
 // 소셜로그인 리다이렉트를 위한 컴포넌트
+import './OAuth2RedirectHandler.css'
 import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import publicApi from '../apis/publicApi';
@@ -18,7 +19,9 @@ const OAuth2RedirectHandler = () => {
             const accessToken = response.headers['authorization'];        
             localStorage.setItem('Authorization', accessToken);
 
-            navigate('/');
+            setTimeout(() => {
+                navigate('/');
+            }, 2000);
         } catch (error) {
             // alert 메시지 (다시 로그인 해주세요 같은 걸로 변경 예정)
             console.error('publicApi에서 에러 발생', error);
@@ -27,7 +30,12 @@ const OAuth2RedirectHandler = () => {
     };
 
     return (
-        <div>로그인 처리 중...</div>
+        <div className='handler-container'>
+            <div className='handler-header'></div>
+            <div className='loading-img'>　　</div>
+            <div className='loading-message'>로그인 중...</div>
+            <div className='handler-bottom'></div>
+        </div>
     );
 };
 
