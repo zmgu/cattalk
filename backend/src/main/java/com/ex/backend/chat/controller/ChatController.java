710c2dbced5b29d4315a6e8182e9423443e24dc4
zmgu <manggu94@gmail.com>
2024-03-27 17:58:33 +0900

    채팅 컨트롤러 생성 (수정 예정)

---
+++ backend/src/main/java/com/ex/backend/chat/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.ex.backend.chat.controller;
+
+import com.ex.backend.chat.dto.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage, SimpMessageHeaderAccessor headerAccessor) {
+        // 사용자를 웹소켓 세션에 추가
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+}
