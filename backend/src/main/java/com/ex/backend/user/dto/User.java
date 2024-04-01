1ff92ddc41a1cc577720325bf110a7e9abb5709b
zmgu <manggu94@gmail.com>
2024-04-01 23:45:04 +0900

    필드 정리

--- backend/src/main/java/com/ex/backend/user/dto/User.java
+++ backend/src/main/java/com/ex/backend/user/dto/User.java
@@ -1,20 +1,24 @@
 package com.ex.backend.user.dto;
 
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
-@Data
+@Getter
+@Setter
+@Builder
 public class User {
 
-    private Long id;
+    private final Long id;
     private String username;
     private String nickname;
     private String password;
     private String role;
     private String name;
     private String email;
-    private Date regDate;
-    private Date updDate;
+    private final Date regDate;
     private int enabled;
 }
