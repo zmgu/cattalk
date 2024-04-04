6b5778175fddea17a5c1e779123383fbdc2dc5cc
zmgu <manggu94@gmail.com>
2024-04-04 22:57:02 +0900

    생성자 추가

--- backend/src/main/java/com/ex/backend/user/dto/User.java
+++ backend/src/main/java/com/ex/backend/user/dto/User.java
@@ -1,23 +1,23 @@
 package com.ex.backend.user.dto;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
-    private final Long userId;
+    private Long userId;
     private String username;
     private String nickname;
     private String password;
     private String role;
     private String name;
     private String email;
-    private final Date regDate;
+    private Date regDate;
     private int enabled;
 }
