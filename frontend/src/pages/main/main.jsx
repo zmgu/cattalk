5a77b32ecf401f8924a158f6ffe4b669927958d0
zmgu <manggu94@gmail.com>
2024-04-04 17:57:55 +0900

    페이지명 변경

--- frontend/src/pages/main/main.jsx
+++ frontend/src/pages/main/main.jsx
@@ -1,7 +1,7 @@
 import './Main.css';
 import MenuBar from '../../components/menuBar/MenuBar';
 import ChatingRoomList from './roomList/ChatingRoomList'
-import FriendsList from './friendsList/FriendsList';
+import FriendsListPage from './friendsList/FriendsListPage';
 import { Routes, Route } from 'react-router-dom';
 
 const Main = () => {
@@ -9,7 +9,7 @@ const Main = () => {
         <div className='main-container'>
             <Routes>
                 <Route path='/' element={ <ChatingRoomList /> } />
-                <Route path='/friendsList' element={ <FriendsList /> } />
+                <Route path='/friends' element={ <FriendsListPage /> } />
             </Routes>
             
             <MenuBar />
