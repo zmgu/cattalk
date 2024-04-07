6a683385bb9fd57de04c9b4d628a475c5c7ce0db
zmgu <manggu94@gmail.com>
2024-04-08 00:28:33 +0900

    채팅방 찾기 api 생성

--- frontend/src/apis/chat.js
+++ frontend/src/apis/chat.js
@@ -1,5 +1,10 @@
 import privateApi from './privateApi';
 
 // 채팅방 생성
-export const createChatRoom = (myUserId, friendUserId, friendNickname) => 
-    privateApi.post(`/chat/room`, { myUserId, friendUserId, friendNickname });
+export const createChatRoom = (myUserId, myNickname, friendUserId, friendNickname) => 
+    privateApi.post(`/chat/room`, { myUserId, myNickname,friendUserId, friendNickname });
+
+
+// 채팅방 찾기
+export const findChatRoom = (myUserId, friendUserId) => 
+    privateApi.get(`/chat/room`, { myUserId, friendUserId });
