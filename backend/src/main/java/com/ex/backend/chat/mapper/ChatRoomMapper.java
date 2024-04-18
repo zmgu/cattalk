3e924b3972c361ec451983141c581c651bad2396
zmgu <manggu94@gmail.com>
2024-04-18 23:23:01 +0900

    채팅방 리스트 매퍼 추가

--- backend/src/main/java/com/ex/backend/chat/mapper/ChatRoomMapper.java
+++ backend/src/main/java/com/ex/backend/chat/mapper/ChatRoomMapper.java
@@ -1,7 +1,10 @@
 package com.ex.backend.chat.mapper;
 
+import com.ex.backend.chat.domain.ChatRoomName;
 import com.ex.backend.chat.dto.FindChatRoomDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface ChatRoomMapper {
@@ -11,4 +14,6 @@ public interface ChatRoomMapper {
     void insertChatRoomParticipant(String roomId, Long userId) throws Exception;
 
     String findChatRoom(Long myUserId, Long friendUserId) throws Exception;
+
+    List<ChatRoomName> findChatRoomList(ChatRoomName chatRoomName) throws Exception;
 }
