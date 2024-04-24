fa158cb9c5fe65db2419baaac116a3c2d2d8f57a
zmgu <manggu94@gmail.com>
2024-04-24 17:56:41 +0900

    findChatRoomName 추가 및 코드 정리

--- frontend/src/apis/chat.js
+++ frontend/src/apis/chat.js
@@ -2,14 +2,32 @@ import privateApi from './privateApi';
 
 // 채팅방 생성
 export const createChatRoom = (myUserId, myNickname, friendUserId, friendNickname) => 
-    privateApi.post(`/chat/room`, { myUserId, myNickname,friendUserId, friendNickname });
+    privateApi.post(`/chat/rooms`, { myUserId, myNickname,friendUserId, friendNickname });
 
+// 내 채팅방 리스트
+export const chatRoomList = (userId) =>
+    privateApi.get(`/chat/rooms`, {
+        params: {
+            userId: userId
+        }
+    }
+);
 
 // 채팅방 찾기
 export const findChatRoom = (myUserId, friendUserId) => 
-    privateApi.get(`/chat/room`, {         
+    privateApi.get(`/chat/rooms/find`, {         
         params: {
         myUserId: myUserId,
         friendUserId: friendUserId
     } 
 });
+
+// 채팅방 이름 찾기
+export const findChatRoomName = (roomId, userId) => 
+    privateApi.get(`/chat/rooms/${roomId}/name`, {
+        params: {
+            roomId: roomId,
+            userId: userId
+        }
+    }
+);
