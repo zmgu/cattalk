cde3b8d85a3a054e743b2e5e1618df9b5bff962b
zmgu <manggu94@gmail.com>
2024-04-24 17:54:30 +0900

    findChatRoomName 추가 및 코드 정리

--- backend/src/main/java/com/ex/backend/chat/controller/ChatController.java
+++ backend/src/main/java/com/ex/backend/chat/controller/ChatController.java
@@ -1,32 +1,45 @@
 package com.ex.backend.chat.controller;
 
+import com.ex.backend.chat.domain.ChatRoomName;
 import com.ex.backend.chat.dto.CreateChatRoomDto;
 import com.ex.backend.chat.service.ChatRoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.logging.Logger;
+import java.util.List;
 
 @RestController
 @RequestMapping("/chat")
 @RequiredArgsConstructor
 public class ChatController {
 
-    private final Logger logger = Logger.getLogger(ChatController.class.getName());
     private final ChatRoomService chatRoomService;
 
-    @PostMapping("/room")
+    @PostMapping("/rooms")
     public ResponseEntity<String> createChatRoom(@RequestBody CreateChatRoomDto createChatRoomDto) {
 
         String createdRoom = chatRoomService.createChatRoom(createChatRoomDto);
         return ResponseEntity.ok(createdRoom);
     }
 
-    @GetMapping("/room")
+    @GetMapping("/rooms")
+    public List<ChatRoomName> findChatRoomList(@RequestParam Long userId) {
+
+        return chatRoomService.findChatRoomList(userId);
+    }
+
+    @GetMapping("/rooms/find")
     public ResponseEntity<String> findChatRoom(@RequestParam Long myUserId, @RequestParam Long friendUserId) {
 
         String roomId = chatRoomService.findChatRoom(myUserId, friendUserId);
         return ResponseEntity.ok(roomId);
+    }
+
+    @GetMapping("/rooms/{roomId}/name")
+    public ResponseEntity<String> findChatRoomName(@PathVariable String roomId, @RequestParam Long userId) {
+        String chatRoomName = chatRoomService.findChatRoomName(roomId, userId);
+
+        return ResponseEntity.ok(chatRoomName);
     }
 }
