6aeb9bdbf48057e0f95e1d4c90ce8dc37f362d24
zmgu <manggu94@gmail.com>
2024-04-24 17:55:38 +0900

    findChatRoomName 추가

--- backend/src/main/java/com/ex/backend/chat/mapper/ChatRoomNameMapper.java
+++ backend/src/main/java/com/ex/backend/chat/mapper/ChatRoomNameMapper.java
@@ -2,10 +2,13 @@ package com.ex.backend.chat.mapper;
 
 import com.ex.backend.chat.domain.ChatRoomName;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface ChatRoomNameMapper {
 
     void createChatRoomName(ChatRoomName chatRoomName) throws Exception;
+
+    String findChatRoomName(@Param("roomId") String roomId, @Param("userId") Long userId) throws Exception;
 
 }
