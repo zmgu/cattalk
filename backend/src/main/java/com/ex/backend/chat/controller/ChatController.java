7fa29a1122ea34293eaae43de3f07f65b880e4d0
zmgu <manggu94@gmail.com>
2024-04-30 17:50:10 +0900

    채팅 메서드 수정중

--- backend/src/main/java/com/ex/backend/chat/controller/ChatController.java
+++ backend/src/main/java/com/ex/backend/chat/controller/ChatController.java
@@ -1,10 +1,15 @@
 package com.ex.backend.chat.controller;
 
+import com.ex.backend.chat.domain.ChatMessage;
 import com.ex.backend.chat.domain.ChatRoomName;
 import com.ex.backend.chat.dto.CreateChatRoomDto;
 import com.ex.backend.chat.service.ChatRoomService;
+import com.ex.backend.chat.service.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +20,7 @@ import java.util.List;
 public class ChatController {
 
     private final ChatRoomService chatRoomService;
+    private final ChatService chatService;
 
     @PostMapping("/rooms")
     public ResponseEntity<String> createChatRoom(@RequestBody CreateChatRoomDto createChatRoomDto) {
@@ -41,5 +47,19 @@ public class ChatController {
         String chatRoomName = chatRoomService.findChatRoomName(roomId, userId);
 
         return ResponseEntity.ok(chatRoomName);
+    }
+
+    @MessageMapping("/sendMessage")
+    @SendTo("/sub/public")  // 메시지를 /sub/public 경로로 방송
+    public ChatMessage sendMessage(@Payload ChatMessage chatMessage) {
+        chatService.saveMessage(chatMessage);
+        return chatMessage;
+    }
+
+    @MessageMapping("/addUser")
+    @SendTo("/sub/public")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage) {
+//        chatMessage.setContent(chatMessage.getSender() + " joined!");
+        return chatMessage;
     }
 }
