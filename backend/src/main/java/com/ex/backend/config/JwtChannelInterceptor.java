e3994ded770b7b34e3d4dc528e77bbd190cb9b29
zmgu <manggu94@gmail.com>
2024-05-28 14:06:00 +0900

    JwtChannelInterceptor 생성

---
+++ backend/src/main/java/com/ex/backend/config/JwtChannelInterceptor.java
@@ -0,0 +1,31 @@
+package com.ex.backend.config;
+
+import com.ex.backend.security.jwt.provider.JwtProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class JwtChannelInterceptor implements ChannelInterceptor {
+
+    private final JwtProvider jwtProvider;
+
+    @Override
+    public Message<?> preSend(Message<?> message, MessageChannel channel) {
+        StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
+        String jwtToken = accessor.getFirstNativeHeader("Authorization");
+
+        if (jwtToken != null && jwtProvider.isExpired(jwtToken)) {
+            Authentication authentication = jwtProvider.getAuthentication(jwtToken);
+            accessor.setUser(authentication);
+        }
+
+        return message;
+    }
+}
+
