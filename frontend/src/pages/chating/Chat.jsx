d153991ef1cc31cd5dac9a81d8d58a25f49a247e
zmgu <122958434+zmgu@users.noreply.github.com>
2024-06-27 18:01:08 +0900

    메시지 말풍선 수정중

--- frontend/src/pages/chating/Chat.jsx
+++ frontend/src/pages/chating/Chat.jsx
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import SockJS from 'sockjs-client';
 import { Client } from '@stomp/stompjs';
 import { faPaperclip, faPaperPlane, faArrowLeft, faMagnifyingGlass, faBars } from '@fortawesome/free-solid-svg-icons';
-
 import { LoginContext } from '../../contexts/LoginContextProvider';
 
 const Chat = () => {
@@ -20,7 +19,17 @@ const Chat = () => {
     const { userInfo } = useContext(LoginContext);
 
     useEffect(() => {
+        if (chatMessageRef.current) {
+            const observer = new MutationObserver(scrollToBottom);
+            observer.observe(chatMessageRef.current, { childList: true });
+            
+            return () => {
+                observer.disconnect();
+            };
+        }
+    }, []);
 
+    useEffect(() => {
         adjustChatMessageHeight();
         window.addEventListener('resize', adjustChatMessageHeight);
 
@@ -73,7 +82,7 @@ const Chat = () => {
         const maxHeight = 200;
         const singleLineHeight = 18;
         const actualHeight = Math.min(textarea.scrollHeight, maxHeight);
-
+    
         if (actualHeight <= singleLineHeight * 2) {
             textarea.style.height = `${singleLineHeight}px`;
         } else {
@@ -81,11 +90,19 @@ const Chat = () => {
         }
         textarea.style.overflowY = textarea.scrollHeight > maxHeight ? 'auto' : 'hidden';
         setMessage(event.target.value);
-
+    
         if (chatInputRef.current) {
             chatInputRef.current.style.height = 'auto';
             const newInputHeight = textarea.offsetHeight + 20;
+            const previousHeight = chatInputRef.current.offsetHeight;
             chatInputRef.current.style.height = `${newInputHeight}px`;
+    
+            // 스크롤 위치를 유지하기 위해 chat-message의 높이 조정
+            if (chatMessageRef.current) {
+                const chatMessageHeight = chatMessageRef.current.offsetHeight;
+                const newChatMessageHeight = chatMessageHeight - (newInputHeight - previousHeight);
+                chatMessageRef.current.style.height = `${newChatMessageHeight}px`;
+            }
         }
     };
 
@@ -105,15 +122,55 @@ const Chat = () => {
                 roomId: roomDetails.roomId,
                 content: message,
                 senderUserId: userInfo.userId,
-                type: 'CHAT'
+                senderNickname: userInfo.nickname,
+                type: 'CHAT',
+                sendTime: new Date().toISOString()
             };
             stompClient.current.publish({ 
                 destination: '/stomp/pub/send', 
-                body: JSON.stringify(chatMessage) });
+                body: JSON.stringify(chatMessage) 
+            });
             setMessage('');
+            
+            if (chatInputRef.current) {
+                const inputElements = chatInputRef.current.getElementsByClassName('message-input');
+                if (inputElements.length > 0) {
+                    inputElements[0].style.height = '18px';  // textarea의 초기 높이 설정
+                }
+                chatInputRef.current.style.height = '50px';  // chat-input의 초기 높이 설정
+            }
+            adjustChatMessageHeight();
+            scrollToBottom(); // 메시지를 보낸 후 스크롤을 맨 아래로 이동
+
+        console.log(messages);
+
         }
     };
 
+    const scrollToBottom = () => {
+        if (chatMessageRef.current) {
+            chatMessageRef.current.scrollTop = chatMessageRef.current.scrollHeight;
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter' && !event.shiftKey) {
+            event.preventDefault();
+            sendMessage();
+        }
+    };
+
+    const formatTime = (timestamp) => {
+        const date = new Date(timestamp);
+        const hours = date.getHours();
+        const minutes = date.getMinutes();
+        const ampm = hours >= 12 ? '오후' : '오전';
+        const formattedHours = hours % 12 || 12; // 0시를 12시로
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : minutes;
+        return `${ampm} ${formattedHours}:${formattedMinutes}`;
+    };
+    
+    
     return (
         <div className='chat-container' ref={chatContainerRef}>
             <div className='chat-header'>
@@ -128,20 +185,56 @@ const Chat = () => {
                     <FontAwesomeIcon icon={faBars} className='chat-header-icon' />
                 </div>
             </div>
-            <div className='chat-message' ref={chatMessageRef}>
+            {/* <div className='chat-message' ref={chatMessageRef}>
                 {messages.map((msg, index) => (
-                    <div key={index} className='message'>
-                        <div className='message-sender'>{msg.senderUserId}</div>
-                        <div className='message-content'>{msg.content}</div>
+                    <div key={index}>
+                        <div className={`${msg.senderUserId === userInfo.userId ? null : 'another-chat-sender'}`}>{msg.senderUserId === userInfo.userId ? null : msg.senderNickname}</div>
+
+                        <div className={`message ${msg.senderUserId === userInfo.userId ? 'my-chat' : 'another-chat'}`}>
+                            <div className='message-content'>{msg.content}</div>
+                        </div>
                     </div>
                 ))}
+            </div> */}
+            <div className='chat-message' ref={chatMessageRef}>
+                {messages.map((msg, index) => {
+                    const isMyMessage = msg.senderUserId === userInfo.userId;
+
+                    const showTime = 
+                        index === messages.length - 1 || 
+                        (messages[index + 1]?.senderUserId !== msg.senderUserId || 
+                        new Date(messages[index + 1]?.sendTime).getMinutes() !== new Date(msg.sendTime).getMinutes());
+
+                    return (
+                        <div key={index} className="chat-message-detail">
+                            {!isMyMessage && (
+                                <div className="another-chat-sender">
+                                    {msg.senderNickname}
+                                </div>
+                            )}
+                            <div className={`message ${isMyMessage ? 'my-chat' : 'another-chat'}`}>
+                                <div className='message-content'>
+                                    {msg.content}
+                                </div>
+                                
+                            </div>
+                            {showTime && (
+                                    <div className={`message-time ${isMyMessage ? 'my-time' : 'another-time'}`}>
+                                        {formatTime(msg.sendTime)}
+                                    </div>
+                                )}
+                        </div>
+                    );
+                })}
             </div>
+
             <div className='chat-input' ref={chatInputRef}>
                 <button className='file-attach-button'><FontAwesomeIcon icon={faPaperclip} /></button>
                 <textarea
                     className='message-input'
                     value={message}
                     onChange={handleMessageChange}
+                    onKeyDown={handleKeyDown}
                     rows={1}
                 />
                 <button className='send-message-button' onClick={sendMessage}><FontAwesomeIcon icon={faPaperPlane} className='chat-input-icon' /></button>
