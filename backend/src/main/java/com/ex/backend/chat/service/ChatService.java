8d9d3acdb877a0496838662b881a5b352c5f8340
zmgu <122958434+zmgu@users.noreply.github.com>
2024-07-04 17:21:14 +0900

    메시지 저장 및 불러오기 추가

--- backend/src/main/java/com/ex/backend/chat/service/ChatService.java
+++ backend/src/main/java/com/ex/backend/chat/service/ChatService.java
@@ -1,20 +1,26 @@
 package com.ex.backend.chat.service;
 
 import com.ex.backend.chat.domain.ChatMessage;
-import com.ex.backend.redis.ChatMessageRedis;
+import com.ex.backend.chat.mapper.ChatMessageMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final ChatMessageRedis chatMessageRedis;
+    private final ChatMessageMapper chatMessageMapper;
+    private final Logger logger = Logger.getLogger(ChatService.class.getName());
 
-    public ChatMessage saveMessage(ChatMessage message) {
-
-        chatMessageRedis.saveChatMessage(message);
-
-        return message;
+    public void saveMessage(ChatMessage chatMessage) {
+        chatMessageMapper.insertChatMessage(chatMessage);
     }
+
+    public List<ChatMessage> getChatMessagesByRoomId(String roomId) {
+        return chatMessageMapper.getChatMessagesByRoomId(roomId);
+    }
+
 }
