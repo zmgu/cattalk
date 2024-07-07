3b7177e14a7077e0f6cab282d42a23f3e104af21
zmgu <122958434+zmgu@users.noreply.github.com>
2024-07-07 15:29:19 +0900

    불필요한 클래스 제거

--- backend/src/main/java/com/ex/backend/chat/entity/ChatRoomParticipantId.java
+++
@@ -1,9 +0,0 @@
-package com.ex.backend.chat.entity;
-
-import java.io.Serializable;
-
-public class ChatRoomParticipantId implements Serializable {
-
-    private String roomId;
-    private Long userId;
-}
