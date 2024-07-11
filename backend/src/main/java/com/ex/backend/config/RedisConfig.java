6e15822d00f0645af5447ace7d1e160674194000
zmgu <122958434+zmgu@users.noreply.github.com>
2024-07-11 17:45:09 +0900

    코드 정리

--- backend/src/main/java/com/ex/backend/config/RedisConfig.java
+++ backend/src/main/java/com/ex/backend/config/RedisConfig.java
@@ -1,6 +1,5 @@
 package com.ex.backend.config;
 
-import com.ex.backend.redis.RedisSubscriber;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,9 +7,7 @@ import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
-import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -24,25 +21,10 @@ public class RedisConfig {
     private int port;
 
     @Bean
-    public RedisMessageListenerContainer redisMessageListenerContainer(
-            RedisConnectionFactory connectionFactory,
-            MessageListenerAdapter listenerAdapter,
-            ChannelTopic channelTopic
-    ){
+    public RedisMessageListenerContainer redisMessageListenerContainer(RedisConnectionFactory connectionFactory){
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
-        container.addMessageListener(listenerAdapter, channelTopic);
         return container;
-    }
-
-    @Bean
-    public MessageListenerAdapter listenerAdapter(RedisSubscriber subscriber) {
-        return new MessageListenerAdapter(subscriber, "onMessage");
-    }
-
-    @Bean
-    public ChannelTopic channelTopic() {
-        return new ChannelTopic("chatroom");
     }
 
     @Bean
