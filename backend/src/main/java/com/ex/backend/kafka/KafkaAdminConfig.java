08f0cde19f56b3447b7b711e29177b0e8376b835
zmgu <122958434+zmgu@users.noreply.github.com>
2024-07-16 17:56:37 +0900

    kafka 설정 클래스 생성 (수정 진행중)

---
+++ backend/src/main/java/com/ex/backend/kafka/KafkaAdminConfig.java
@@ -0,0 +1,33 @@
+package com.ex.backend.kafka;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaAdminConfig {
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
+
+        return new KafkaAdmin(configs);
+    }
+
+    @Bean
+    public NewTopic chatFunctionTopic() {
+        return TopicBuilder.name("chat")
+                .partitions(10)
+                .replicas(1)
+                .build();
+    }
+}
