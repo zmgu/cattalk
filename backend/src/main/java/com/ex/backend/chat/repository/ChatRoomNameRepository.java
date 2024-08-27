d8248e24b85d81c7a9a6f13d54dbe76efae65cee
zmgu <122958434+zmgu@users.noreply.github.com>
2024-08-27 17:53:06 +0900

    코드 정리

--- backend/src/main/java/com/ex/backend/chat/repository/ChatRoomNameRepository.java
+++ backend/src/main/java/com/ex/backend/chat/repository/ChatRoomNameRepository.java
@@ -4,11 +4,7 @@ import com.ex.backend.chat.entity.ChatRoomName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface ChatRoomNameRepository extends JpaRepository<ChatRoomName, String> {
-
-    List<ChatRoomName> findByUserId(Long userId);
-
 }
