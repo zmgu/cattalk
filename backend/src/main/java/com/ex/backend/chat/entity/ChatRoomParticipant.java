dee4a796ba7bcc4095777df1ea845c5ccbcd9a1a
zmgu <122958434+zmgu@users.noreply.github.com>
2024-09-17 17:52:19 +0900

    ChatRoomParticipant 생성

--- backend/src/main/java/com/ex/backend/chat/entity/ChatRoomParticipant.java
+++ backend/src/main/java/com/ex/backend/chat/entity/ChatRoomParticipant.java
@@ -3,20 +3,24 @@ package com.ex.backend.chat.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "chat_room_name")
-public class ChatRoomName {
+@Table(name = "chat_room_participant")
+public class ChatRoomParticipant {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long chatRoomNameId;
+    private Long chatRoomParticipantId;
     private String roomId;
     private Long userId;
     private String roomName;
+    private Date lastMessageReadAt;
+    private Date JoinedAt;
 
 }
