81b0eab997bd109baddaffb6cfebc5ecccfabc28
zmgu <122958434+zmgu@users.noreply.github.com>
2024-09-17 18:04:24 +0900

    웹소켓 연결, 해제 시의 로직 추가

--- backend/src/main/java/com/ex/backend/websocket/WebSocketEventListener.java
+++ backend/src/main/java/com/ex/backend/websocket/WebSocketEventListener.java
@@ -1,30 +1,60 @@
 package com.ex.backend.websocket;
 
-import com.ex.backend.chat.service.ChatService;
+import com.ex.backend.chat.service.ChatRoomService;
+import com.ex.backend.security.jwt.provider.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.messaging.SessionConnectEvent;
 import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
+import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
 @Component
 @RequiredArgsConstructor
 public class WebSocketEventListener {
 
-    private final ChatService chatService;
+    private final JwtProvider jwtProvider;
+    private final ChatRoomService chatRoomService;
+    private final WebSocketSessionManager webSocketSessionManager;
     private final Logger logger = Logger.getLogger(WebSocketEventListener.class.getName());
 
     /*
+    *  웹소켓 연결된 후 같은 웹소켓 연결된 유저에게 내 정보 브로드캐스트
+    * */
     @EventListener
     public void handleWebSocketConnectListener(SessionConnectEvent event) {
-        logger.info("handleWebSocketConnectListener 동작중");
-    }
-    */
+        StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(event.getMessage());
+        List<String> accessToken = headerAccessor.getNativeHeader("Authorization");
+        List<String> roomIdHeader = headerAccessor.getNativeHeader("roomId");
 
+        Long userId = jwtProvider.getUserId(accessToken.get(0));
+        String roomId = roomIdHeader.get(0);
+        logger.info("이벤트 리스너 시작 userId : " + userId);
+        chatRoomService.broadcastLastReadTime(roomId, userId);
+    }
+
+    /*
+     *  채팅방을 나가면서 웹소켓 세션 삭제, 내 Redis 정보 업데이트, RDB 읽은 시간 업데이트
+     *  만약 내 세션이 채팅방의 마지막 세션이었다면 채팅방 정보 전체를 RDB 읽은 시간 업데이트 후 Redis 삭제
+     * */
     @EventListener
     public void handleWebSocketDisconnectListener(SessionDisconnectEvent event) {
-        logger.info("webSocketDisconnectListener 동작중");
+        String sessionId = event.getSessionId();
+        String roomId = webSocketSessionManager.getRoomIdBySessionId(sessionId);
+        Long userId = webSocketSessionManager.getUserIdBySessionId(sessionId);
+
+        chatRoomService.updateLastReadTimeRedis(userId, roomId, new Date());
+        webSocketSessionManager.removeUserSession(sessionId);
+
+        if(webSocketSessionManager.getRoomAllUserByRoomId(roomId).size() < 1) {
+            logger.info("채팅방 웹소켓 연결된 유저가 없음 -> 채팅방 redis 전체 삭제");
+            chatRoomService.deleteChatRoomRedis(roomId);
+        }
+        logger.info("채팅방 : " + roomId + " 접속 유저 목록 : " + webSocketSessionManager.getRoomAllUserByRoomId(roomId));
+
     }
 }
