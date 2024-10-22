163fc1a47ede9eec030cbe20b2fb76bdc22e5874
zmgu <122958434+zmgu@users.noreply.github.com>
2024-10-22 17:46:36 +0900

    다양한 클래스 타입의 ConcurrentKafkaListenerContainerFactory 생성할 수 있도록 수정

--- backend/src/main/java/com/ex/backend/kafka/KafkaConsumerConfig.java
+++ backend/src/main/java/com/ex/backend/kafka/KafkaConsumerConfig.java
@@ -1,57 +1,50 @@
 package com.ex.backend.kafka;
 
 import com.ex.backend.chat.entity.ChatMessage;
+import com.google.common.collect.ImmutableMap;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
-import org.springframework.kafka.listener.ContainerProperties;
-import org.springframework.kafka.listener.MessageListener;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 @EnableKafka
 public class KafkaConsumerConfig {
 
     @Bean
-    public ConsumerFactory<String, ChatMessage> consumerFactory() {
-        Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "default-group-id");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class.getName()); // 클래스 이름으로 변경
-
-        // 신뢰할 수 있는 패키지 설정
-        props.put(JsonDeserializer.TRUSTED_PACKAGES, "com.ex.backend.chat.entity");
-        return new DefaultKafkaConsumerFactory<>(props);
+    public ConcurrentKafkaListenerContainerFactory<String, ChatMessage> chatKafkaListenerContainerFactory() {
+        return getContainerFactory("chat", ChatMessage.class);
     }
 
-    @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, ChatMessage> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, ChatMessage> factory = new ConcurrentKafkaListenerContainerFactory<>();
-        factory.setConsumerFactory(consumerFactory());
-
-        // 초기 오프셋 설정
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL_IMMEDIATE);
-        factory.getContainerProperties().setSyncCommits(true);
-
+    private <T> ConcurrentKafkaListenerContainerFactory<String, T> getContainerFactory(String groupId, Class<T> classType) {
+        ConcurrentKafkaListenerContainerFactory<String, T> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(getKafkaConsumerFactory(groupId, classType));
         return factory;
     }
 
-    public ConcurrentMessageListenerContainer<String, ChatMessage> createContainer(String topic, String groupId,
-                                                                                   MessageListener<String, ChatMessage> listener) {
-        ContainerProperties containerProps = new ContainerProperties(topic);
-        containerProps.setGroupId(groupId);
-        containerProps.setMessageListener(listener);
+    private <T> DefaultKafkaConsumerFactory<String, T> getKafkaConsumerFactory(String groupId,Class<T> classType) {
+        JsonDeserializer<T> deserializer = setDeserializer(classType);
+        return new DefaultKafkaConsumerFactory<>(setConfig(groupId, deserializer), new StringDeserializer(), deserializer);
+    }
 
-        return new ConcurrentMessageListenerContainer<>(consumerFactory(), containerProps);
+    private <T> ImmutableMap<String, Object> setConfig(String groupId, JsonDeserializer<T> deserializer) {
+        ImmutableMap<String, Object> config = ImmutableMap.<String, Object>builder()
+                .put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9094")
+                .put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class)
+                .put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
+                .put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, deserializer)
+                .put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG,"1")
+                .build();
+        return config;
+    }
+
+    private <T> JsonDeserializer<T> setDeserializer(Class<T> classType) {
+        JsonDeserializer<T> deserializer = new JsonDeserializer<>(classType, false);
+        return deserializer;
     }
 }
