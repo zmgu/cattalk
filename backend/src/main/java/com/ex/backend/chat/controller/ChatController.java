4127c4b31af49b30924d12e1ff4f29b47132e81e
zmgu <122958434+zmgu@users.noreply.github.com>
2024-10-22 17:46:22 +0900

    코드 정리

--- backend/src/main/java/com/ex/backend/chat/controller/ChatController.java
+++ backend/src/main/java/com/ex/backend/chat/controller/ChatController.java
@@ -2,7 +2,6 @@ package com.ex.backend.chat.controller;
 
 import com.ex.backend.chat.entity.ChatMessage;
 import com.ex.backend.chat.service.ChatService;
-import com.ex.backend.kafka.service.KafkaConsumerService;
 import com.ex.backend.kafka.service.KafkaProducerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +17,6 @@ public class ChatController {
 
     private final ChatService chatService;
     private final KafkaProducerService kafkaProducerService;
-    private final KafkaConsumerService kafkaConsumerService;
 
     @GetMapping("/chat/rooms/{roomId}/messages")
     public ResponseEntity<List<ChatMessage>> getChatMessages(@PathVariable String roomId) {
@@ -29,7 +27,6 @@ public class ChatController {
     @MessageMapping("/send")
     public void sendMessage(@Payload ChatMessage chatMessage) {
         kafkaProducerService.sendMessage(chatMessage);
-        kafkaConsumerService.startListenerForRoom(chatMessage);
         chatService.saveMessage(chatMessage);
     }
 
